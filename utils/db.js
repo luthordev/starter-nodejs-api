828df5c894ddc77930fb7f55c4045c62a039fdf9
Luthfi Athorique Ginanjar Putra <luthfiathorique@gmail.com>
2023-06-13 20:59:57 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -2,441 +2,62 @@ const { PrismaClient, Prisma } = require("@prisma/client");
 const prisma = new PrismaClient();
 const message = require("../messages");
 
-let models = {};
+const models = {};
 
-Object.keys(prisma)
-  .filter((p) => !p.includes("_") && !p.includes("$"))
-  .map((key) => {
+Object.keys(prisma).forEach((key) => {
+  if (!key.includes("_") && !key.includes("$")) {
     models[key] = {
-      findUnique: async (param) => {
-        try {
-          let result = await prisma[key].findUnique(param);
-          if (result)
-            return {
-              status: "success",
-              result,
-            };
-
-            return {
-              status: "failed",
-              message: message.GET_NOT_FOUND,
-            };
-        } catch (e) {
-          console.log(e);
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
-      findUniqueOrThrow: async (param) => {
-        try {
-          let result = await prisma[key].findUniqueOrThrow(param);
-
-          if (result)
-            return {
-              status: "success",
-              result,
-            };
-        } catch (e) {
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
-      findMany: async (param) => {
-        try {
-          let result = await prisma[key].findMany(param);
-
-          if (result)
-            return {
-              status: "success",
-              result,
-            };
-        } catch (e) {
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
-      upsert: async (param) => {
-        try {
-          let result = await prisma[key].upsert(param);
-
-          if (result)
-            return {
-              status: "success",
-              result,
-            };
-        } catch (e) {
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
-      aggregate: async (param) => {
-        try {
-          let result = await prisma[key].aggregate(param);
-
-          if (result)
-            return {
-              status: "success",
-              result,
-            };
-        } catch (e) {
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
-      groupBy: async (param) => {
-        try {
-          let result = await prisma[key].groupBy(param);
-
-          if (result)
-            return {
-              status: "success",
-              result,
-            };
-        } catch (e) {
-          console.log(e);
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
-      count: async () => {
-        try {
-          let result = await prisma[key].count();
-
-          if (result)
-            return {
-              status: "success",
-              count: result,
-            };
-        } catch (e) {
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
-      create: async (param) => {
-        try {
-          let result = await prisma[key].create(param);
-
-          if (result)
-            return {
-              status: "success",
-              message: message.ADD_SUCCESS,
-              created_Data: result,
-            };
-        } catch (e) {
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
-      createMany: async (param) => {
-        try {
-          let result = await prisma[key].createMany(param);
-
-          if (result)
-            return {
-              status: "success",
-              message: message.ADD_SUCCESS,
-              created_Data: result,
-            };
-        } catch (e) {
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
-      update: async (param) => {
-        try {
-          let result = await prisma[key].update(param);
-
-          if (result)
-            return {
-              status: "success",
-              message: message.UPDATE_SUCCESS,
-              updated_Data: result,
-            };
-        } catch (e) {
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
-      updateMany: async (param) => {
-        try {
-          let result = await prisma[key].updateMany(param);
-
-          if (result)
-            return {
-              status: "success",
-              message: message.UPDATE_SUCCESS,
-              updated_Data: result,
-            };
-        } catch (e) {
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
-      delete: async (param) => {
-        try {
-          let result = await prisma[key].delete(param);
-          console.log(result);
-          if (result)
-            return {
-              status: "success",
-              message: message.DELETE_SUCCESS,
-              deleted_Data: result,
-            };
-        } catch (e) {
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
-      deleteMany: async (param) => {
-        try {
-          let result = await prisma[key].deleteMany(param);
-
-          if (result)
-            return {
-              status: "success",
-              message: message.DELETE_SUCCESS,
-              deleted_Data: result,
-            };
-        } catch (e) {
-          if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            const promises = Object.keys(message.PRISMA_ERRORS).map(
-              (errCode, i) => {
-                if (e.code === errCode) {
-                  return {
-                    status: "failed",
-                    code: errCode,
-                    message: message.PRISMA_ERRORS[errCode],
-                    ...e.meta,
-                  };
-                }
-              }
-            );
-            const errors = await Promise.all(promises);
-            return errors.find((error) => error !== undefined);
-          } else
-            return {
-              status: "failed",
-              message: message.UNKNOWN_ERROR,
-            };
-        }
-      },
+      findUnique: createPrismaWrapper(prisma[key].findUnique),
+      findUniqueOrThrow: createPrismaWrapper(prisma[key].findUniqueOrThrow),
+      findMany: createPrismaWrapper(prisma[key].findMany),
+      upsert: createPrismaWrapper(prisma[key].upsert),
+      aggregate: createPrismaWrapper(prisma[key].aggregate),
+      groupBy: createPrismaWrapper(prisma[key].groupBy),
+      count: createPrismaWrapper(prisma[key].count),
+      create: createPrismaWrapper(prisma[key].create),
+      createMany: createPrismaWrapper(prisma[key].createMany),
+      update: createPrismaWrapper(prisma[key].update),
+      updateMany: createPrismaWrapper(prisma[key].updateMany),
+      delete: createPrismaWrapper(prisma[key].delete),
+      deleteMany: createPrismaWrapper(prisma[key].deleteMany),
     };
-  });
+  }
+});
+
+function createPrismaWrapper(prismaMethod) {
+  return async (param) => {
+    try {
+      const result = await prismaMethod(param);
+      if (result) {
+        return {
+          status: "success",
+          result,
+        };
+      }
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        const promises = Object.keys(message.PRISMA_ERRORS).map(
+          (errCode) => {
+            if (e.code === errCode) {
+              return {
+                status: "failed",
+                code: errCode,
+                message: message.PRISMA_ERRORS[errCode],
+                ...e.meta,
+              };
+            }
+          }
+        );
+        const errors = await Promise.all(promises);
+        return errors.find((error) => error !== undefined);
+      } else {
+        return {
+          status: "failed",
+          message: message.UNKNOWN_ERROR,
+        };
+      }
+    }
+  };
+}
 
 module.exports = models;
